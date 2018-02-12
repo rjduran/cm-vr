f374aa51e72a9c05a965f70b185a1a6eb4abcf4a
RJ Duran <rjduranjr@gmail.com>
2018-02-12 11:49:00 -0700

    added Example_08 to demonstrate a simple teleporter

---
+++ CMVR_Scripting/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+    // Position of target (Cube B) with small offset (to avoid teleport loop)
+    private Vector3 target1 = new Vector3(4f,0.6f,0f); // Cube B
+    private Vector3 target2 = new Vector3(-4f, 0.6f, 0f); // Cube A
+
+    private void OnTriggerEnter(Collider col)
+    {
+        //Debug.Log("trigger enter");
+        Debug.Log(col.gameObject.name + " has entered " + name);
+
+        // One Way Teleport (Cube A -> Cube B)
+        // Set the position of my (Sphere) object to the target position (Cube B) 
+        // when it collides with Cube A.
+        //col.gameObject.transform.position = target1;
+
+        // Two Way Teleport
+        // Set the position of my (Sphere) object to one of the target positions
+        // depending on which cube it collides with. 
+        if (name == "Cube A") {
+            col.gameObject.transform.position = target1;
+        }
+
+        if (name == "Cube B") {
+            col.gameObject.transform.position = target2;
+        }
+
+    }
+
+}
