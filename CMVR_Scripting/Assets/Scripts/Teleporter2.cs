3eb6455eb1b93ec58c8a3593ac89ca7ebfa40e2b
RJ Duran <rjduranjr@gmail.com>
2018-02-12 17:35:58 -0700

    added teleporter examples including multiples and simple randomizer

---
+++ CMVR_Scripting/Assets/Scripts/Teleporter2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter2 : MonoBehaviour
+{
+
+    // Position of target (Cube B) with small offset (to avoid teleport loop)
+    private Vector3 target2 = new Vector3(-4f, 0.6f, 0f); // Cube A
+    private Vector3 target1 = new Vector3(4f, 0.6f, 0f); // Cube B
+    private Vector3 target3 = new Vector3(0f, 0.6f, 4f); // Cube C
+    private Vector3 target4 = new Vector3(0f, 0.6f, -4f); // Cube D
+
+    private void OnTriggerEnter(Collider col)
+    {
+        //Debug.Log("trigger enter");
+        Debug.Log(col.gameObject.name + " has entered " + name);
+
+        if (name == "Cube A")
+        {
+            col.gameObject.transform.position = target1;
+        }
+
+        if (name == "Cube B")
+        {
+            col.gameObject.transform.position = target2;
+        }
+
+        if (name == "Cube C")
+        {
+            col.gameObject.transform.position = target4;
+        }
+
+        if (name == "Cube D")
+        {
+            col.gameObject.transform.position = target3;
+        }
+
+    }
+
+}
---
+++ CMVR_Scripting/Assets/Scripts/TeleporterRandom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterRandom : MonoBehaviour {
+
+    private float rand;
+    private Vector3[] pos;
+
+	// Use this for initialization
+	void Start () {     
+        // Define the side of the array before putting values into it
+        pos = new Vector3[4];
+
+        // Fill array with Vector3 objects
+        pos[0] = new Vector3(-3f, 0.6f, 0f); // Cube A
+        pos[1] = new Vector3(3f, 0.6f, 0f); // Cube B
+        pos[2] = new Vector3(0f, 0.6f, 3f); // Cube C
+        pos[3] = new Vector3(0f, 0.6f, -3f); // Cube D
+        		
+	}
+	
+    private void OnTriggerEnter(Collider col)
+    {
+        //Debug.Log("trigger enter");
+        Debug.Log(col.gameObject.name + " has entered " + name);
+
+        rand = Random.Range(0, pos.Length);
+        //Debug.Log(rand); // Print out the random number
+        //Debug.Log(pos[(int)rand]); // Print out the value of pos
+
+        // Set the position of the Sphere
+        col.gameObject.transform.position = pos[(int)rand];
+    }
+}
